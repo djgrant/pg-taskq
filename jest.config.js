d1af42e7fe373845971c8ade495e6c640c0b60c8
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2020-07-03 21:27:09 +0100

    Set up test suite

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  globalSetup: "<rootDir>/node_modules/@databases/pg-test/jest/globalSetup.js",
+  globalTeardown:
+    "<rootDir>/node_modules/@databases/pg-test/jest/globalTeardown.js",
+};
