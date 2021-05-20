58b2c78c43520c57b1d5488d91bded13c00e68a8
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2021-05-20 17:37:59 +0100

    Serve sub routes statically

--- packages/pg-taskq-dashboard/server/express.js
+++ packages/pg-taskq-dashboard/server/express.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const express = require("express");
 
-const pgTaskqDashboard = express.static(path.join(__dirname, "../build"));
+const static = express.static(path.join(__dirname, "../build"));
+const pgTaskqDashboard = express();
+
+pgTaskqDashboard.use(static);
+pgTaskqDashboard.use("/*", static);
 
 module.exports = { pgTaskqDashboard };
